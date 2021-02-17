3d526d03260c7dd442915d53f0f5051fc92fb5d8
Birkhoff <zhiqic2@illinois.edu>
2021-02-18 00:52:23 +0800

    Implement periodic CPU time update
    Use top half (timer interrupt) and bottom half (workqueue work)
    Also add spinlock to all write operation to the process list.

--- mp1/mp1.c
+++ mp1/mp1.c
@@ -6,6 +6,8 @@
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
+#include <linux/workqueue.h>
+#include <linux/spinlock.h>
 #include "mp1_given.h"
 
 MODULE_LICENSE("GPL");
@@ -16,11 +18,37 @@ MODULE_DESCRIPTION("CS-423 MP1");
 #define FILENAME "status"
 static struct proc_dir_entry *mp1_dir, *status_file;
 LIST_HEAD(mp1_process_list);
+static struct timer_list mp1_timer;
+static struct workqueue_struct *wq = NULL;
+static DEFINE_SPINLOCK(mp1_list_lock);
 struct mp1_process {
 	pid_t pid;
 	unsigned long cpu_use;
 	struct list_head elem;
 };
+
+void update_cpu_time(struct work_struct *work) {
+	struct mp1_process *proc, *tmp;
+	unsigned long cpu_time, flags;
+	list_for_each_entry_safe(proc, tmp, &mp1_process_list, elem) {
+		if (get_cpu_use(proc->pid, &cpu_time) == 0) {
+			proc->cpu_use = cpu_time;
+		}
+		else {
+			spin_lock_irqsave(&mp1_list_lock, flags);
+			list_del(&proc->elem);
+			spin_unlock_irqrestore(&mp1_list_lock, flags);
+			kfree(proc);
+		}
+	}
+}
+
+DECLARE_WORK(mp1_work, update_cpu_time);
+
+void enq_work(unsigned long arg) {
+	queue_work(wq, &mp1_work);
+	mod_timer(&mp1_timer, jiffies + msecs_to_jiffies(5000));
+}
 
 static ssize_t mp1_read(struct file *file, char __user *buffer, size_t count, loff_t *data) {
 	return 0;
@@ -31,6 +59,7 @@ static ssize_t mp1_write(struct file *file, const char __user *buffer, size_t co
 	pid_t pid;
 	int error;
 	struct mp1_process *proc;
+	unsigned long flags;
 	char *kbuf = kmalloc(count + 1, GFP_KERNEL);
 	if (kbuf == NULL) {
 		bytes_written = -ENOMEM;
@@ -47,7 +76,9 @@ static ssize_t mp1_write(struct file *file, const char __user *buffer, size_t co
 	proc = kmalloc(sizeof(struct mp1_process), GFP_KERNEL);
 	proc->pid = pid;
 	proc->cpu_use = 0;
+	spin_lock_irqsave(&mp1_list_lock, flags);
 	list_add_tail(&proc->elem, &mp1_process_list);
+	spin_unlock_irqrestore(&mp1_list_lock, flags);
 
 free_and_out:
 	kfree(kbuf);
@@ -80,6 +111,11 @@ int __init mp1_init(void)
 		goto out;
 	}
 
+	setup_timer(&mp1_timer, enq_work, 0);
+	mod_timer(&mp1_timer, jiffies + msecs_to_jiffies(5000));
+
+	wq = create_singlethread_workqueue("mp1");
+
 out:
 	return error;
 }
@@ -93,6 +129,11 @@ void __exit mp1_exit(void)
 		list_del(&proc->elem);
 		kfree(proc);
 	}
+
+	del_timer(&mp1_timer);
+
+	flush_workqueue(wq);
+	destroy_workqueue(wq);
 
 	remove_proc_entry("status", mp1_dir);
 	remove_proc_entry("mp1", NULL);
