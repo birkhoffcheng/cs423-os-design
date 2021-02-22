495abb4a87d187d610b1dc665d0cbe8cb751f8d1
Birkhoff <zhiqic2@illinois.edu>
2021-02-22 21:17:21 +0800

    Implement userapp

--- mp1/userapp.c
+++ mp1/userapp.c
@@ -2,5 +2,11 @@
 
 int main(int argc, char* argv[])
 {
+	pid_t pid = getpid();
+	FILE *status_file = fopen("/proc/mp1/status", "w");
+	if (status_file) {
+		fprintf(status_file, "%d", pid);
+		fclose(status_file);
+	}
 	return 0;
 }
