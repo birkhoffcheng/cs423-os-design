ddfdea19e5bcf7ddd321703f82c034fbd305c0bb
Birkhoff <zhiqic2@illinois.edu>
2021-03-01 23:11:27 +0800

    Implement user factorial app

--- mp1/userapp.c
+++ mp1/userapp.c
@@ -8,5 +8,21 @@ int main(int argc, char* argv[])
 		fprintf(status_file, "%d", pid);
 		fclose(status_file);
 	}
+	uint64_t n, i, random;
+	time_t start = time(NULL);
+	while (time(NULL) - start < 10) {
+		random = rand() % 64;
+		n = 1;
+		for (i = 1; i <= random; i++)
+			n *= i;
+		printf("%lu! = %lu\n", random, n);
+	}
+	int fd = open("/proc/mp1/status", O_RDONLY);
+	if (fd >= 0) {
+		char buffer[BUFSIZ];
+		read(fd, buffer, BUFSIZ);
+		puts(buffer);
+		close(fd);
+	}
 	return 0;
 }
--- mp1/userapp.h
+++ mp1/userapp.h
@@ -1,3 +1,8 @@
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <time.h>
