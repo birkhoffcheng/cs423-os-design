b2650c327c8128f5c08540428d5e4c6ec1f61e16
Birkhoff <zhiqic2@illinois.edu>
2021-03-22 12:09:32 -0500

    Add userapp

--- mp2/userapp.c
+++ mp2/userapp.c
@@ -1,5 +1,45 @@
 #include "userapp.h"
 
 int main(int argc, char **argv) {
-	return 0;
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s <period> <runtime>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	pid_t pid = getpid();
+	int period = atoi(argv[1]);
+	int runtime = atoi(argv[2]);
+	if (period == 0 || runtime == 0) {
+		fprintf(stderr, "Period and runtime must be integers\n");
+		return EXIT_FAILURE;
+	}
+	FILE *fp = fopen("/proc/mp2/status", "w");
+	if (!fp) {
+		fprintf(stderr, "Status file opening failed\n");
+		return EXIT_FAILURE;
+	}
+	fprintf(fp, "R, %d, %d, %d", pid, period, runtime);
+	fclose(fp);
+	char buf[BUFSIZ];
+	sprintf(buf, "test $(grep %d /proc/mp2/status | wc -l) -gt 0", pid);
+	if (system(buf) != 0) {
+		fprintf(stderr, "Registration failed\n");
+		return EXIT_FAILURE;
+	}
+	int i;
+	unsigned long random, n;
+	time_t wakeup_time, process_time;
+	srand(time(NULL));
+	fp = fopen("/proc/mp2/status", "w");
+	for (i = rand() & 0xFF; i > 0; i--) {
+		wakeup_time = time(NULL);
+		n = 0;
+		for (random = rand() % 64; random >= 0; random--)
+			n *= random;
+		process_time = time(NULL) - wakeup_time;
+		printf("wakeup: %d, process: %d\n", wakeup_time, process_time);
+		fprintf(fp, "Y, %d", pid);
+	}
+	fprintf(fp, "D, %d", pid);
+	fclose(fp);
+	return EXIT_SUCCESS;
 }
--- mp2/userapp.h
+++ mp2/userapp.h
@@ -1,2 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <time.h>
